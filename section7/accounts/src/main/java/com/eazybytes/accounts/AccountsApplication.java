b92dd4fcce406e66d6a12be904b03af3353985c7
Aniket Chavan <anikettchavan103@gmail.com>
2025-11-11 20:16:43 +0530

    Section 7 - Using MySQL DBs inside microservices

---
+++ section7/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,19 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountContactInfoDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountContactInfoDto.class})
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+		System.out.println("Welcome!..");
+	}
+
+}
